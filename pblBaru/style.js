edb4184ae617fab1250b00b14177190af25ad93a
asepjak <septianprogaming@gmail.com>
2024-07-30 08:46:24 +0700

    Delete pblBaru directory

--- pblBaru/style.js
+++
@@ -1,9 +0,0 @@
-// window.addEventListener("load", function () {
-//   setTimeout(function open(event) {
-//     document.querySelector(".popup-container").style.display = "block";
-//   }, 500);
-// });
-
-// document.querySelector("#close").addEventListener("click", function () {
-//   document.querySelector(".popup-container").style.display = "none";
-// });
